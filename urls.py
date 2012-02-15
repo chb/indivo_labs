94b43ee45110874291ebbc5fad73a891f9bcf568
Travers Franckle <travers.franckle@childrens.harvard.edu>
2012-02-15 09:38:51 -0500

    initial labs app update

--- urls.py
+++ urls.py
@@ -1,18 +1,24 @@
 from django.conf.urls.defaults import *
 from views import *
 import settings
+from django.conf import settings as rootsettings
 
 # all this is under apps/labs
 urlpatterns = patterns('',
     # authentication
     (r'^start_auth', start_auth),
     (r'^after_auth', after_auth),
-    (r'^lab$', list_labs), # can't use "list" obviously
+    (r'^lab/(?P<lab_id>[^/]+)/$', show_lab),
+    (r'^labs$', list_labs), 
+    # static
+    ## WARNING NOT FOR PRODUCTION
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': rootsettings.SERVER_ROOT_DIR + settings.STATIC_HOME}),
     # (r'^labs/new$', new_med),
     # (r'^labs/(?P<med_id>[^/]+)', one_med),
     # (r'^$', lambda request: index()),
     (r'^jmvc/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.JMVC_HOME}),
     (r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.JS_HOME})
+    
 )
 
 
