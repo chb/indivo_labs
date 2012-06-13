114a14c6dbf6bc118aa51b6c2bc61d88bf29297d
Daniel Haas <daniel.haas@childrens.harvard.edu>
2012-06-13 17:55:16 +0000

    Updated app for new python client and new SMART datamodels

--- views.py
+++ views.py
@@ -5,10 +5,17 @@
 from lxml import etree
 from utils import *
 from django.shortcuts import render_to_response
+from django.utils import simplejson
 import settings # app local
 import dateutil.parser
 
 NS = 'http://indivo.org/vocab/xml/documents#'
+
+LAB_STATUSES = {
+    'correction': 'Correction',
+    'preliminary': 'Preliminary',
+    'final': 'Final',
+}
 
 def start_auth(request):
     """
@@ -31,16 +38,14 @@ def start_auth(request):
     if carenet_id:
         params['indivo_carenet_id'] = carenet_id
     
-    params['offline'] = 1
-    
     # request a request token
-    request_token = parse_token_from_response(client.post_request_token(data=params))
-    
+    req_token = client.fetch_request_token(params)
+
     # store the request token in the session for when we return from auth
-    request.session['request_token'] = request_token
+    request.session['request_token'] = req_token
     
     # redirect to the UI server
-    return HttpResponseRedirect(settings.INDIVO_UI_SERVER_BASE + '/oauth/authorize?oauth_token=%s' % request_token['oauth_token'])
+    return HttpResponseRedirect(client.auth_redirect_url)
 
 def after_auth(request):
     """
@@ -59,11 +64,8 @@ def after_auth(request):
     # get the indivo client and use the request token as the token for the exchange
     client = get_indivo_client(request, with_session_token=False)
     client.update_token(token_in_session)
-    
-    # create the client
-    params = {'oauth_verifier' : oauth_verifier}
-    access_token = parse_token_from_response(client.post_access_token(data=params))
-    
+    access_token = client.exchange_token(oauth_verifier)
+
     # store stuff in the session
     request.session['access_token'] = access_token
     
@@ -76,67 +78,71 @@ def after_auth(request):
             del request.session['record_id']
         request.session['carenet_id'] = access_token['xoauth_indivo_carenet_id']
     
-    # now get the long-lived token using this access token
-    client= get_indivo_client(request)
-    try:
-        long_lived_token = parse_token_from_response(client.get_long_lived_token())
-        
-        request.session['long_lived_token'] = long_lived_token
-    except:
-        pass
     return index(request)
 
 def index(request):
     return list_labs(request)
 
-def get_labs(root):
-    labs = []
-    reportNodes = root.findall('.//Report')
-        
-    for r in reportNodes:
-        d = r.findtext('.//{%s}dateMeasured' % NS, default='')
+def parse_labs(labs):
+    
+    def _process_lab(lab):
+        lab['id'] = lab['__documentid__']
+        del lab['__documentid__']
+
+        # Parse the lab's date
         try:
-            # parse date string and set timezone as UTC
-            d = dateutil.parser.parse(d)
+            d = dateutil.parser.parse(lab['collected_at'])
             d = d.astimezone(dateutil.tz.tzutc())
         except ValueError as e:
             d = 'parse error'
-            
-        lab = {
-                 'dateMeasured': d,
-                 'labType': r.findtext('.//{%s}labType' % NS, default=''),
-                 'laboratory': {
-                        'name': r.findtext('.//{%s}name' % NS, default=''),
-                        'address': r.findtext('.//{%s}address' % NS, default='')
-                         },
-                 'comments': r.findtext('.//{%s}comments' % NS, default=''),
-                 'firstLabTestName': r.findtext('.//{%s}firstLabTestName' % NS, default=''),
-                 'firstLabTestValue': r.findtext('.//{%s}firstLabTestValue' % NS, default=''),
-                 'normalRangeMinimum': r.findtext('.//{%s}normalRangeMinimum' % NS, default=''),
-                 'normalRangeMaximum': r.findtext('.//{%s}normalRangeMaximum' % NS, default=''),
-                 # meta information
-                 'id': r.find('.//Document').attrib['id']
-        }
+        lab['collected_at'] = d
         
+        # Normalize the lab's status text
+        if lab['status_identifier'] in LAB_STATUSES:
+            lab['status_title'] = LAB_STATUSES[lab['status_identifier']]
+        else:
+            lab['status_title'] = 'Unknown'
+
+        # Determine if the lab is abnormal
         try:
             # See if we can determine if value is outside the normal min/max
-            test_value = float(lab.firstLabTestValue)
-            min = float(lab.normalRangeMinimum)
-            max = float(lab.normalRangeMaximum)
+            test_value = float(lab['quantitative_result_value_value'])
+            min = float(lab['quantitative_result_normal_range_min_value'])
+            max = float(lab['quantitative_result_normal_range_max_value'])
             if test_value > max or test_value < min:
-                lab.update({'abnormal': True})
-        except Exception as e:
+                lab['abnormal'] = True
+
+            # labs are also abnormal if they are explicitly labeled as such
+            abn_interp = lab.get('abnormal_interpretation_identifier', None)
+            if abn_interp and abn_interp != 'normal':
+                lab['abnormal'] = True
+        except (KeyError, ValueError) as e:
             pass
+
+        # Preprocess the lab's address and organization names
+        lab['org'] = lab['collected_by_org_name'] or 'Not Supplied'
+        prefix = 'collected_by_org_adr_'
+        adr_fields = (
+            lab[prefix+'street'],
+            lab[prefix+'city'],
+            lab[prefix+'region'],
+            lab[prefix+'postalcode'],
+            lab[prefix+'country'],
+            )
+        lab['adr'] = ', '.join([f for f in adr_fields if f]) or 'Not Supplied'
+
+        return lab
         
-        labs.append(lab)
-        
-    return labs
+    return map(_process_lab, labs)
 
 def show_lab(request, lab_id):
     client = get_indivo_client(request)
     record_id = request.session['record_id']
-    doc = client.read_document(record_id=record_id, document_id=lab_id)
-    lab = etree.fromstring(doc.response['response_data'])
+    resp, doc = client.record_specific_document(record_id=record_id, document_id=lab_id)
+    if resp['status'] != '200':
+        # TODO: handle errors
+        raise Exception("Error loading original lab document: %s"%doc)
+    lab = etree.fromstring(doc)
     return render_to_response("labs/templates/show.html", {'lab':etree.tostring(lab, pretty_print=True), 
                                                            'STATIC_HOME': settings.STATIC_HOME}
     )
@@ -145,31 +151,29 @@ def list_labs(request):
     # read in query params
     limit = int(request.GET.get('limit', 15))
     offset = int(request.GET.get('offset', 0))
-    order_by = request.GET.get('order_by', 'date_measured') # lab_test_name, lab_type, created_at, date_measured
-    lab_type = request.GET.get('lab_type', 'All')
-    
-    
+    order_by = request.GET.get('order_by', 'collected_at') # test_name_title, created_at, collected_at
+    lab_status = request.GET.get('lab_status', 'All') # final, corrected, preliminary
+    lab_status_display = LAB_STATUSES.get(lab_status, 'All')
+
     #read in previous values from session
     previous_date_start = request.session.get("date_start", None)
     previous_date_end = request.session.get("date_end", None)
 
     if request.session.has_key('record_id'):
         record_id = request.session['record_id']
-        # retrieve list of lab_types for this record
-        lab_types = []
         client = get_indivo_client(request)
-        lab_type_list = client.read_labs(record_id = record_id, parameters = {'group_by': 'lab_type', 'aggregate_by': 'count*lab_type'}).response['response_data']
-        lab_type_root = etree.XML(lab_type_list)
-        for aggregateReport in lab_type_root.findall('.//{%s}AggregateReport' % NS):
-            lab_types.append(aggregateReport.attrib['group'])
-        lab_types.sort()
+
         # retrieve a min date for labs
-        oldest_params = {'limit': '1', 'order_by': 'date_measured'}
-        if lab_type in lab_types:
-            oldest_params.update({'lab_type': lab_type})
-        oldest_lab = get_labs(etree.XML(client.read_labs(record_id = record_id, parameters = oldest_params).response['response_data']))
+        oldest_params = {'limit': '1', 'order_by': 'collected_at'}
+        if lab_status in LAB_STATUSES:
+            oldest_params['status_identifier'] = lab_status
+        resp, content = client.generic_list(record_id=record_id, data_model="LabResult", body=oldest_params)
+        if resp['status'] != '200':
+            # TODO: handle errors
+            raise Exception("Error fetching oldest lab: %s"%content)
+        oldest_lab = parse_labs(simplejson.loads(content))
         if len(oldest_lab) > 0:
-            oldest_lab_date = oldest_lab[0]['dateMeasured']
+            oldest_lab_date = oldest_lab[0]['collected_at']
         else:
             oldest_lab_date = datetime.datetime.utcnow()
 
@@ -188,31 +192,31 @@ def list_labs(request):
         
         # set params for lab query    
         parameters = {'limit': limit, 'offset': offset, 'order_by': order_by}
-        if lab_type in lab_types:
-            parameters.update({'lab_type': lab_type})
-        parameters.update({'date_range': 'date_measured*' + date_start_string + '*' + date_end_string})
-        labs_xml = client.read_labs(record_id = record_id, parameters = parameters).response['response_data']
-        #TODO: check response status
+        parameters.update({'date_range': 'collected_at*' + date_start_string + '*' + date_end_string})
+        if lab_status in LAB_STATUSES:
+            parameters['status_identifier'] = lab_status
+        resp, content = client.generic_list(record_id=record_id, data_model="LabResult", body=parameters)
+        if resp['status'] != '200':
+            # TODO: handle errors
+            raise Exception("Error fetching labs: %s"%content)
+        labs = parse_labs(simplejson.loads(content))
     else:
         #TODO: not the case anymore
         print 'FIXME: no client support for labs via carenet. See problems app for an example.. Exiting...'
         return
     
-    # parse labs
-    labs_root = etree.XML(labs_xml)
-    labs = get_labs(labs_root)
-    
     # build a description for the range of results shown and calculate offsets
     next_offset = None
     prev_offset = None
-    total_document_count = int(labs_root.find('.//Summary').attrib['total_document_count'])
-    if total_document_count <= 0:
+    num_labs = len(labs)
+    if num_labs == 0 and offset == 0:
         range_description = 'No Results'
-    elif offset + limit < total_document_count:
-        next_offset = offset + limit
-        range_description = 'Showing ' + str(offset + 1) + '-' + str(offset + limit) + ' of ' +  str(total_document_count)
+    elif num_labs == 0:
+        range_description = 'End of Results'
     else:
-         range_description = 'Showing ' + str(offset + 1) + '-' + str(total_document_count) + ' of ' +  str(total_document_count)
+        range_description = 'Showing Results ' + str(offset + 1) + '-' + str(offset + num_labs)
+        if limit == num_labs:
+            next_offset = offset + limit
          
     if offset > 0:
         prev_offset = offset - limit
@@ -220,16 +224,17 @@ def list_labs(request):
             prev_offset = 0
             
     return render_to_response("labs/templates/list.html", {'labs': labs, 
-                                                           'lab_types': lab_types,
+                                                           'lab_statuses': LAB_STATUSES,
                                                            'STATIC_HOME': settings.STATIC_HOME,
                                                            'order_by': order_by,
                                                            'limit': limit,
                                                            'offset': offset,
-                                                           'lab_type': lab_type,
+                                                           'lab_status_id': lab_status,
+                                                           'lab_status_display': lab_status_display,
                                                            'next_offset': next_offset,
                                                            'prev_offset': prev_offset,
                                                            'range_description': range_description,
-                                                           'total_document_count': total_document_count,
+                                                           'num_labs': num_labs,
                                                            'min_date': min_date_string,
                                                            'max_date': max_date_string,
                                                            'date_start': date_start_string,
